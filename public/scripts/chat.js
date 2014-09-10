acd13f597431ec34714fc739aae7a183f7698416
zhangsq <zhangsq@outlook.com>
2014-09-10 19:26:51 +0800

    页面JS文件

---
+++ public/scripts/chat.js
@@ -0,0 +1,37 @@
+var Chat = function (socket) {
+	this.socket = socket;
+};
+Chat.prototype.sendMessage = function (room, text) {
+	var message = {
+		room : room,
+		text : text
+	};
+	this.socket.emit('message', message);
+};
+Chat.prototype.changeRoom = function (room) {
+	this.socket.emit('join', {
+		newRoom : room
+	});
+};
+Chat.prototype.processCommand = function (command) {
+	var words = command.split(' '),
+		command = words[0].substring(1, words[0].length).toLowerCase(),
+		message, room, name;
+
+	switch(command) {
+		case 'join':
+			words.shift();
+			room = words.join(' ');
+			this.changeRoom(room);
+			break;
+		case 'nick':
+			words.shift();
+			name = words.join(' ');
+			this.socket.emit('nameAttempt', name);
+			break;
+		default:
+			message = 'Unrecognized command.';
+			break;
+	}
+	return message;
+};
